0d41f1c31f117f3a74b95771fa465170c3a9e7ad
Ranjan Shrestha <ranjan@sharingapples.com>
2017-12-08 23:25:23 +0545

    Android: Load the native dependencies automatically Automatically detect and install react-native packages that have native android packages without using rnpm or react-native-link

--- android/app/src/main/java/com/foundation/MainApplication.java
+++ android/app/src/main/java/com/foundation/MainApplication.java
@@ -7,9 +7,16 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+
+class ManuallyConfiguredException extends Exception {
+  ManuallyConfiguredException(String msg) {
+    super(msg);
+  }
+}
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -21,9 +28,40 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
+
+      ArrayList<ReactPackage> packages = new ArrayList<>();
+      packages.add(new MainReactPackage());
+      packages.add(new RNDeviceInfo());
+
+      // Load the packages from the native dependencies
+      for (String reactPackage:BuildConfig.REACT_PACKAGES) {
+        try {
+          final Class<ReactPackage> LibReactPackage = (Class<ReactPackage>) this.getClass().getClassLoader().loadClass(reactPackage);
+
+          // In case the package has already been instantiated, just ignore it
+          for (ReactPackage rp : packages) {
+            if (LibReactPackage.isInstance(rp)) {
+              System.out.println();
+              throw new ManuallyConfiguredException("ReactPackage" + reactPackage + " configured manually");
+            }
+          }
+
+          // See if we have a default constructor, in which case instantiate one
+          packages.add(LibReactPackage.newInstance());
+
+        } catch (ClassNotFoundException e) {
+          System.err.println("Error loading react package " + reactPackage);
+          System.err.println(e);
+        } catch (InstantiationException e) {
+          System.err.println("The ReactPackage" + reactPackage + " needs to be configured manually. Please check the library documentation.");
+        } catch (IllegalAccessException e) {
+          System.err.println("The ReactPackage" + reactPackage + " needs to be configured manually. Please check the library documentation.");
+        } catch (ManuallyConfiguredException err) {
+          System.out.println("INFO::" + err.getMessage());
+        }
+      }
+
+      return packages;
     }
 
     @Override
